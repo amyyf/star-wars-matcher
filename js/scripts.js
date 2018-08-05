366cf2bf1267ebb5a11390cd5caca8f72ffdd111
Amy Frieson <ava447@gmail.com>
2018-08-05 17:14:16 -0400

    Refactor fetch to circumvent data issues

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,16 @@
 /* global fetch */
-/* Starships are commented out due to data issue */
-(function () {
+(function (apiURL) {
   const model = {
     init: function () {
       const model = this;
       this.people = [];
       this.planets = [];
       this.species = [];
-      // this.starships = [];
+      this.starships = [];
       this.vehicles = [];
-      fetch('https://swapi.co/api/')
+      fetch(apiURL)
         .then(function (response) {
-          response.ok ? model.ok = true : model.ok = false; // check if API is up
+          model.ok = response.ok; // check if API is up
           return response.ok;
         })
         .then(function (response) {
@@ -19,18 +18,31 @@
         })
         .catch(e => console.log(e));
     },
-    fetchInstance: function (type, num) {
+    fetchInstance: function (type, num, index) {
       const model = this;
+      let reqURL = apiURL;
       // check if data already exists in model
-      if (model[type][num]) {
-        return Promise.resolve(model[type][num]);
+      if (model[type][index]) {
+        return Promise.resolve(model[type][index]);
       }
-      // get data if it hasn't already been requested and store it for later reuse
-      return fetch(`https://swapi.co/api/${type}/${num}/`)
+      if (num <= 10) {
+        reqURL += `${type}/`;
+      } else if (num <= 20) {
+        reqURL += `${type}/?page=2`;
+        index -= 10;
+      } else if (num <= 30) {
+        reqURL += `${type}/?page=3`;
+        index -= 20;
+      } else {
+        reqURL += `${type}/?page=4`;
+        index -= 30;
+      }
+      return fetch(reqURL)
         .then(response => response.json())
-        .then(function (result) {
-          model[type][num] = result;
-          return model[type][num];
+        .then(function (response) {
+          const instance = response.results[index];
+          model[type][index] = instance;
+          return instance;
         })
         .catch(function (e) {
           console.log(e);
@@ -41,12 +53,13 @@
   const controller = {
     init: function () {
       const controller = this;
-      controller.num = 0;
+      controller.num = 0; // day of month of birthday
+      controller.index = 0; // array index will be one less than day
       model.init();
       view.init();
     },
     getDataByType: function (type) {
-      return model.fetchInstance(type, this.num);
+      return model.fetchInstance(type, this.num, this.index);
     },
     setApiStatus: function (status) {
       this.isWorking = status;
@@ -56,6 +69,7 @@
     },
     updateBirthdate: function () {
       this.num = view.dateInput.value;
+      this.index = this.num - 1; // set array index
     }
   };
 
@@ -66,7 +80,7 @@
       this.personBtn = document.getElementById('person');
       this.planetBtn = document.getElementById('planet');
       this.speciesBtn = document.getElementById('species');
-      // this.starshipBtn = document.getElementById('starship');
+      this.starshipBtn = document.getElementById('starship');
       this.vehicleBtn = document.getElementById('vehicle');
 
       this.dateInput.addEventListener('change', controller.updateBirthdate.bind(controller));
@@ -82,10 +96,10 @@
         controller.getDataByType('species')
           .then(species => view.renderSpecies(species));
       });
-      // this.starshipBtn.addEventListener('click', function () {
-      //   controller.getDataByType('starships')
-      //     .then(starship => view.renderStarship(starship));
-      // });
+      this.starshipBtn.addEventListener('click', function () {
+        controller.getDataByType('starships')
+          .then(starship => view.renderStarship(starship));
+      });
       this.vehicleBtn.addEventListener('click', function () {
         controller.getDataByType('vehicles')
           .then(vehicle => view.renderVehicle(vehicle));
@@ -109,12 +123,12 @@
       `;
       this.display.innerHTML = speciesDisplay;
     },
-    // renderStarship: function (starship) {
-    //   const starshipDisplay = `
-    //     <h2>${starship.name}</h2>
-    //   `;
-    //   this.display.innerHTML = starshipDisplay;
-    // },
+    renderStarship: function (starship) {
+      const starshipDisplay = `
+        <h2>${starship.name}</h2>
+      `;
+      this.display.innerHTML = starshipDisplay;
+    },
     renderVehicle: function (vehicle) {
       const vehicleDisplay = `
         <h2>${vehicle.name}</h2>
@@ -130,4 +144,4 @@
     }
   };
   controller.init();
-})();
+})('https://swapi.co/api/');
