f0f1b02df455473c26c8b69bf55b952e71bf0fc3
Amy Frieson <ava447@gmail.com>
2018-08-05 08:56:21 -0400

    Refactor getData call for reusability

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,10 @@
     init: function () {
       const model = this;
       this.people = [];
+      this.planets = [];
+      this.species = [];
+      this.starships = [];
+      this.vehicles = [];
       fetch('https://swapi.co/api/')
         .then(function (response) {
           response.ok ? model.ok = true : model.ok = false; // check if API is up
@@ -14,12 +18,13 @@
         })
         .catch(e => console.log(e));
     },
-    getData: function (type, num) {
+    fetchInstance: function (type, num) {
       const model = this;
       // check if data already exists in model
       if (model[type][num]) {
         return Promise.resolve(model[type][num]);
       }
+      // get data if it hasn't already been requested and store it for later reuse
       return fetch(`https://swapi.co/api/${type}/${num}/`)
         .then(response => response.json())
         .then(function (result) {
@@ -36,12 +41,11 @@
     init: function () {
       const controller = this;
       controller.num = 0;
-      controller.index = 0;
       model.init();
       view.init();
     },
-    getPerson: function () {
-      return model.getData('people', this.num);
+    getData: function (type) {
+      return model.fetchInstance(type, this.num);
     },
     setApiStatus: function (status) {
       this.isWorking = status;
@@ -51,7 +55,6 @@
     },
     updateBirthdate: function () {
       this.num = view.dateInput.value;
-      this.index = this.num - 1; // array index will be one digit lower than day of month
     }
   };
 
@@ -62,7 +65,7 @@
       this.dateInput.addEventListener('change', controller.updateBirthdate.bind(controller));
       this.personBtn = document.getElementById('person');
       this.personBtn.addEventListener('click', function () {
-        controller.getPerson()
+        controller.getData('people')
           .then(person => view.renderPerson(person));
       });
     },
