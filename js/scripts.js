3302460653c536b3f79b43576149e2bb543465cd
Amy Frieson <ava447@gmail.com>
2018-08-05 07:26:21 -0400

    Add real data back in

--- js/scripts.js
+++ js/scripts.js
@@ -1,53 +1,26 @@
 /* global fetch */
 (function () {
   const model = {
-    // init: function () {
-    //   fetch('https://swapi.co/api/people/')
-    //     .then(response => response.json())
-    //     .then(function (response) {
-    //       this.peopleList = response;
-    //       console.log(this.peopleList);
-    //       return this.peopleList;
-    //     })
-    //     .catch(function (e) {
-    //       console.log(e);
-    //     });
-    // }
-    peopleList: [
-      {
-        'birth_year': '19 BBY',
-        'eye_color': 'Blue',
-        'films': [
-          'https://swapi.co/api/films/1/'
-        ],
-        'gender': 'Male',
-        'hair_color': 'Blond',
-        'height': '172',
-        'homeworld': 'https://swapi.co/api/planets/1/',
-        'mass': '77',
-        'name': 'Luke Skywalker',
-        'skin_color': 'Fair',
-        'created': '2014-12-09T13:50:51.644000Z',
-        'edited': '2014-12-10T13:52:43.172000Z',
-        'species': [
-          'https://swapi.co/api/species/1/'
-        ],
-        'starships': [
-          'https://swapi.co/api/starships/12/'
-        ],
-        'url': 'https://swapi.co/api/people/1/',
-        'vehicles': [
-          'https://swapi.co/api/vehicles/14/'
-        ]
-      }
-    ]
+    init: function () {
+      const model = this;
+      fetch('https://swapi.co/api/people/')
+        .then(response => response.json())
+        .then(function (response) {
+          model.peopleList = response.results;
+          console.log(model.peopleList);
+          return model.peopleList;
+        })
+        .catch(function (e) {
+          console.log(e);
+        });
+    }
   };
 
   const controller = {
     init: function () {
       this.num = 0;
       this.index = 0;
-      // model.init();
+      model.init();
       view.init();
     },
     getPerson: function () {
@@ -55,7 +28,7 @@
     },
     updateBirthdate: function () {
       this.num = view.dateInput.value;
-      this.index = this.num - 1; // array index will be one digit lower
+      this.index = this.num - 1; // array index will be one digit lower than day of month
     }
   };
 
@@ -66,7 +39,7 @@
       this.dateInput.addEventListener('change', controller.updateBirthdate.bind(controller));
       this.personBtn = document.getElementById('person');
       this.personBtn.addEventListener('click', function () {
-        const person = controller.getPerson.bind(controller)();
+        const person = controller.getPerson();
         view.renderPerson(person);
       });
     },
